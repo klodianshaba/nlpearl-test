1219cf2fe447ab7c4c5484c80e818ba7bf43933f
klodian shaba <dev.klodian@gmail.com>
2024-06-11 12:59:02 +0200

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,5 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:'', loadComponent: () => import('./pages/test/test.component').then(m => m.TestComponent)}
+];
