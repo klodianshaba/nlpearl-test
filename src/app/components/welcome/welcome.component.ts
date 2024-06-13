64540a26ed7132a312a98d155d647007e939bdd7
klodian shaba <dev.klodian@gmail.com>
2024-06-13 10:03:51 +0200

    welcome and unlock the power of NLPearl

--- src/app/components/welcome/welcome.component.ts
+++ src/app/components/welcome/welcome.component.ts
@@ -5,8 +5,9 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './welcome.component.html',
-  styleUrl: './welcome.component.scss'
+  styleUrl: './welcome.component.scss',
 })
 export class WelcomeComponent {
-
+  companyName = 'NLPearl';
+  welcome = `Unlock the Power of Conversation with ${this.companyName} and Transform Your Business Interactions`;
 }
